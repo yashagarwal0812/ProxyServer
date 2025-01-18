5b8577ff59581371e26207bded4a84f94fa6a3fa
Yash <yashagarwal827327@gmail.com>
2025-01-18 10:52:37 +0530

    added raii classes

--- with_cache.cpp
+++ with_cache.cpp
@@ -25,6 +25,64 @@
 
 using namespace std;
 
+class PthreadMutexLock
+{
+private:
+    pthread_mutex_t *mutex;
+
+public:
+    explicit PthreadMutexLock(pthread_mutex_t &m)
+    {
+        mutex = &m;
+        pthread_mutex_lock(mutex);
+    }
+
+    ~PthreadMutexLock()
+    {
+        pthread_mutex_unlock(mutex);
+    }
+};
+
+class SemaphoreWrapper
+{
+private:
+    sem_t *sem;
+    string name;
+
+public:
+    SemaphoreWrapper(const char *sem_name, int value)
+    {
+        name = sem_name;
+        sem_unlink(sem_name);
+        sem = sem_open(sem_name, O_CREAT | O_EXCL, 0644, value);
+        if (sem == SEM_FAILED)
+        {
+            throw runtime_error("Failed to create semaphore");
+        }
+    }
+
+    ~SemaphoreWrapper()
+    {
+        sem_close(sem);
+        sem_unlink(name.c_str());
+    }
+
+    void wait()
+    {
+        sem_wait(sem);
+    }
+
+    void post()
+    {
+        sem_post(sem);
+    }
+
+    sem_t *get()
+    {
+        return sem;
+    }
+};
+
 class CacheElement
 {
 private:
